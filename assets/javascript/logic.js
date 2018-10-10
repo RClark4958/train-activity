21c0fc4fc43c463a8ccfa3557ddb5e9c965ca581
Rclark4598 <rickdclark48@gmail.com>
2018-10-10 16:51:57 -0600

    polished_logic

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -12,12 +12,16 @@
 
   var trainData = firebase.database();
 
-  $("#addTrainBtn").on("click", function() {
+  $("#addTrainBtn").on("click",function(){
+    
+    
+
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
     var firstTrain = moment($("#firstTrainInput").val().trim(),"HH:mm").subtract(10,"years").format("X");
     var frequency = $("#frequencyInput").val().trim();
 
+   
     var newTrain = {
       name: trainName,
       destination: destination,
@@ -37,4 +41,17 @@
     
     return false;
 
-  })
+  });
+
+  trainData.ref().on("child_added", function(snapshot) {
+    var name = snapshot.val().name;
+    var destination = snapshot.val().destination;
+    var frequency = snapshot.val().frequency;
+    var firstTrain = snapshot.val().firstTrain;
+
+    var remainder = moment().diff(moment.unix(firstTrain), "minutes")%frequency;
+    var minutes = frequency - remainder;
+    var arrival = moment().add(minutes,"m").format("hh:mm A");
+
+    $("#trainTable > tBody").append("<tr><td>"+name+"</td><td>"+destination+"</td><td>"+frequency+"</td><td>"+arrival+"</td><td>"+minutes+"</td></tr>");
+  });
